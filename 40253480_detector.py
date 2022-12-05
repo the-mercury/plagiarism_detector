597c01ff9d7498fd39ada32b4946d6cec928e358
Mehrdad Asadi <mehrdadasadi91@gmail.com>
2022-12-05 14:37:21 -0500

    [enhancement] removing redundant code and optimizing frequency map construction

--- 40253480_detector.py
+++ 40253480_detector.py
@@ -5,7 +5,7 @@ import sys
 
 class Utilities:
     COMMON = [
-        'a', 'an', '',
+        'a', 'an',
         'i', 'you', 'he', 'she', 'they', 'we',
         'am', 'is', 'are', 'be',
         'my', 'his', 'her', 'their', 'ours',
@@ -26,18 +26,7 @@ class Utilities:
     def text_preprocess(text: str) -> [str]:
         text = text.replace('\n', '')
         text = text.replace('\t', '')
-        return text.lower().translate(str.maketrans('', '', string.punctuation)).split(sep=' ')
-
-    @staticmethod
-    def frequency_mapping(word_list: list) -> dict:
-        frequency_dict = {}
-        for new_word in word_list:
-            if new_word not in Utilities.COMMON:
-                if new_word in frequency_dict:
-                    frequency_dict[new_word] = frequency_dict[new_word] + 1
-                else:
-                    frequency_dict[new_word] = 1
-        return frequency_dict
+        return text.lower().translate(str.maketrans('', '', string.punctuation)).split()
 
     @staticmethod
     def vector_magnitude(vector: list) -> float:
@@ -52,19 +41,19 @@ class Utilities:
         return [vector[i]/vector_magnitude for i in range(len(vector))]
 
     @staticmethod
-    def vector_similarity(freq_dict_1: dict, freq_dict_2: dict, threshold: float = 0.5) -> bool:
-        vector_1 = Utilities.normalize_vector(list(freq_dict_1.values()))
-        vector_2 = Utilities.normalize_vector(list(freq_dict_2.values()))
+    def vector_similarity(vector_1: list, vector_2: list, threshold: float = 0.5) -> bool:
         dif = Utilities.vector_magnitude([(vector_2[i] - vector_1[i]) for i in range(len(vector_1))])
         plus = Utilities.vector_magnitude([(vector_2[i] + vector_1[i]) for i in range(len(vector_1))])
         return dif / plus < threshold
 
     @staticmethod
-    def word_dict(set_union: set, freq_dict: dict) -> dict:
+    def word_dict(set_union: set, word_list: list) -> dict:
         word_dict = {word: 0 for word in set_union}
-        for key in word_dict:
-            if key in freq_dict.keys():
-                word_dict[key] = freq_dict[key]
+        for word in word_list:
+            if word in Utilities.COMMON:
+                word_dict[word] += 0.1
+            else:
+                word_dict[word] += 1
         return word_dict
 
 
@@ -73,19 +62,19 @@ def detect_plagiarism() -> bool:
     path_2 = sys.argv[2]
     file_1 = Utilities.read_file(path_1)
     list_1 = Utilities.text_preprocess(file_1)
-    freq_dict_1 = Utilities.frequency_mapping(list_1)
     file_2 = Utilities.read_file(path_2)
     list_2 = Utilities.text_preprocess(file_2)
-    freq_dict_2 = Utilities.frequency_mapping(list_2)
-    set_union_keys = set(freq_dict_1.keys()).union(set(freq_dict_2.keys()))
-    word_dict_1 = Utilities.word_dict(set_union_keys, freq_dict_1)
-    word_dict_2 = Utilities.word_dict(set_union_keys, freq_dict_2)
-    similarity = Utilities.vector_similarity(word_dict_1, word_dict_2)
+    set_union_keys = set(list_1).union(set(list_2))
+    word_dict_1 = Utilities.word_dict(set_union_keys, list_1)
+    word_dict_2 = Utilities.word_dict(set_union_keys, list_2)
+    vector_1 = Utilities.normalize_vector(list(word_dict_1.values()))
+    vector_2 = Utilities.normalize_vector(list(word_dict_2.values()))
+    similarity = Utilities.vector_similarity(vector_1, vector_2)
     return similarity
 
 
 if __name__ == '__main__':
     try:
         print(1) if detect_plagiarism() else print(0)
-    except:
+    except Exception:
         print(-1)
